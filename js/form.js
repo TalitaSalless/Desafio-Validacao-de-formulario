2ab64dd638affb14675ad55c1eb007ef57729503
Talita Salles <talita_salles9@outlook.com>
2023-01-18 19:01:33 -0300

    ajustando

--- js/form.js
+++ js/form.js
@@ -1,14 +1,16 @@
 const campos = document.querySelectorAll(".campoDoFormulario");
-const spans = document.querySelectorAll('.span-erro')
+const spans = document.querySelectorAll(".span-erro");
 const botaoEnviar = document.querySelector("#btnEnviar");
 const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
 
-botaoEnviar.addEventListener('click', function() {
-   preventDefault()
-   campos.forEach(function (input) {
+botaoEnviar.addEventListener('click', () => {
+   event.preventDefault()
+   campos.forEach((input) => {
       if (input.value == "") {
          input.classList.add("campo-vazio");
+         
       } else if (input.value !== "") {
+         
          input.classList.remove("campo-vazio");
          input.classList.add("campo-preenchido");
       }
